4c316ea24782f2917eb35e072dd502b9ebb93618
madz371 <chmadz45@gmail.com>
2024-07-29 20:49:35 +0700

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -4,7 +4,7 @@ exports.routes = {
    path: '/',
    method: 'get',
    execution: async (req, res, next) => {
-      res.sendFile(path.join(__dirname, '/lib/index.html'));
+      res.sendFile(path.join(__dirname, './lib/index.html'));
      /*res.json({
          creator: global.creator,
          msg: {
