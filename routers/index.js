28be0554d9f6ddfe5bb038dc52dfb1b8fce6c34e
madz371 <chmadz45@gmail.com>
2024-07-29 21:01:30 +0700

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -1,46 +1,15 @@
 const quoteApi = require('@neoxr/quote-api')
-const app = express();
-const path = require('path');
-const port = 3000;
-const fs = require('fs');
 exports.routes = {
    category: 'main',
    path: '/',
    method: 'get',
    execution: async (req, res, next) => {
-     /*res.json({
+      res.json({
          creator: global.creator,
          msg: {
-            message: 'LO KONTOL!'
+            error: 'LO KONTOL!, NGAPAIN?'
          }
-      })*/
-       if (req.method === 'GET') {
-        if (req.url === '/') {
-            // Serve the HTML file
-            const filePath = path.join(__dirname, './lib/index.html');
-            fs.readFile(filePath, 'utf8', (err, data) => {
-                if (err) {
-                    res.status(500).send('Internal Server Error');
-                } else {
-                    res.setHeader('Content-Type', 'text/html');
-                    res.send(data);
-                }
-            });
-        } else if (req.url === '/api/quote') {
-            // Serve the API response
-            res.setHeader('Content-Type', 'application/json');
-            res.json({
-                creator: global.creator,
-                msg: {
-                    message: 'LO KONTOL!'
-                }
-            });
-        } else {
-            res.status(404).send('Not Found');
-        }
-    } else {
-        res.status(405).send('Method Not Allowed');
-    }
+      })
    },
    error: false
 }
