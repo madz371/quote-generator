2d5d73fef0c807dfec5553a09198f6bdebf433bf
madz371 <chmadz45@gmail.com>
2024-07-29 20:59:00 +0700

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -2,6 +2,7 @@ const quoteApi = require('@neoxr/quote-api')
 const app = express();
 const path = require('path');
 const port = 3000;
+const fs = require('fs');
 exports.routes = {
    category: 'main',
    path: '/',
@@ -13,7 +14,33 @@ exports.routes = {
             message: 'LO KONTOL!'
          }
       })*/
-      res.sendFile(path.join(__dirname, 'index.html'));
+       if (req.method === 'GET') {
+        if (req.url === '/') {
+            // Serve the HTML file
+            const filePath = path.join(__dirname, './lib/index.html');
+            fs.readFile(filePath, 'utf8', (err, data) => {
+                if (err) {
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    res.setHeader('Content-Type', 'text/html');
+                    res.send(data);
+                }
+            });
+        } else if (req.url === '/api/quote') {
+            // Serve the API response
+            res.setHeader('Content-Type', 'application/json');
+            res.json({
+                creator: global.creator,
+                msg: {
+                    message: 'LO KONTOL!'
+                }
+            });
+        } else {
+            res.status(404).send('Not Found');
+        }
+    } else {
+        res.status(405).send('Method Not Allowed');
+    }
    },
    error: false
 }
